5226dca15904f440933595eb14448c46f8668ea7
nt2311-vn <nguyentoan231196@gmail.com>
2025-11-10 11:17:40 +0700

    add annotation context autoconfigure SpringBootApplication

--- src/main/java/com/packt/albums/AlbumsApplication.java
+++ src/main/java/com/packt/albums/AlbumsApplication.java
@@ -1,6 +1,7 @@
 package com.packt.albums;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class AlbumsApplication {
