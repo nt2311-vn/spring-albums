6aceaa47837bcf7d5b5b8100d53473a38edbbc86
nt2311-vn <nguyentoan231196@gmail.com>
2025-11-10 11:17:57 +0700

    config value for AlbumsConfigation and return a restClient

--- src/main/java/com/packt/albums/AlbumsConfiguration.java
+++ src/main/java/com/packt/albums/AlbumsConfiguration.java
@@ -1,6 +1,17 @@
 package com.packt.albums;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestClient;
 
 @Configuration
-public class AlbumsConfiguration {}
+public class AlbumsConfiguration {
+  @Value("${football.api.url: http://localhost:8080}")
+  String basURI;
+
+  @Bean
+  RestClient restClient() {
+    return RestClient.create(basURI);
+  }
+}
